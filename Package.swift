93e73fbf5af75474e808dbde1d834a0e3d75502f
cloudwebrtc <duanweiwei1982@gmail.com>
2025-08-20 11:53:30 +0800

    bump version to 137.7151.03.

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/webrtc-sdk/Specs/releases/download/137.7151.02/WebRTC.xcframework.zip",
-            checksum: "3956d18169b718c3a37dfe520c5de75a4b206d94bf714e8ef0d2f62ab94e6d78"
+            url: "https://github.com/webrtc-sdk/Specs/releases/download/137.7151.03/WebRTC.xcframework.zip",
+            checksum: "150052870008ba9d077d0904c5ffe8689f0ce649dbecf4cc740cc1b913d9dcf5"
         ),
     ]
 )
